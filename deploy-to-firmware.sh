b60969e53f1448ead1b7829a7f6014ee023f8f33
Daniel Mack <daniel@zonque.org>
2021-05-14 17:51:33 +0200

    add script to deploy to firmware webroot

---
+++ deploy-to-firmware.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+dest=$1
+
+if [ ! -d "$dest" ]; then
+	echo "Usage: $0 <firmware-data-dir>"
+	exit 1
+fi
+
+rm -rf dist
+npm run build
+
+find dist -type f -name *legacy* | xargs -r rm
+find dist -type f | xargs -rn 1 gzip
+
+rm -rf $dest/webroot
+mkdir $dest/webroot
+cp -av dist/* $dest/webroot/
+
+du -sh dist
+
