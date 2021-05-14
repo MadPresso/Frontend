8b8a5cb27b400a8b16901970460b3de16bf08063
Daniel Mack <daniel@zonque.org>
2021-05-14 17:51:18 +0200

    remove purgecss

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,7 @@ const path = require('path');
 
 module.exports = {
   configureWebpack: {
-    plugins: [
-      new PurgecssPlugin({
-        paths: glob.sync([
-          path.join(__dirname, './node_modules/buefy/**/*.@(vue|js|ts)'),
-          path.join(__dirname, './public/index.html'),
-          path.join(__dirname, './src/**/*.@(vue|js|ts)')
-        ]),
-        // safelist: [/icon/, /button/, /is-grouped/, /^navbar-/, /^has-text-/, /^mdi-/, /^has-numberinput-/, /select/, /switch/, /modal/, /b-tabs/, /button/, /autocomplete/, /dropdown/],
-      })
-    ],
+    plugins: [],
     devServer: {
       proxy: {
         '/api': {
